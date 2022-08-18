95c12de3ab720df1d273958e67f5669f8a506696
esraa <cse.20.003@student.uotechnology.edu.iq>
2022-08-18 02:54:11 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dnatask/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dnatask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dnatask/page1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+ // runApp(MaterialApp(home:page1() ,));
+  runApp(MaterialApp(home:page2() ,));
+
+}
+class screen extends StatefulWidget {
+  const screen({Key? key}) : super(key: key);
+
+  @override
+  State<screen> createState() => _screenState();
+}
+
+class _screenState extends State<screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+ 
